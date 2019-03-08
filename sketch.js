f3488a34289c8c7013ed66d8e0575a9cd052848e
jordanherbert <40042885+jordanherbert@users.noreply.github.com>
2019-03-08 14:53:41 -0800

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -10,14 +10,14 @@ function setup() {
 	can.position(windowWidth/8,windowHeight/8);
 	can.class("bord");
 
-	p=createP('Click play to start the magic. This is Sierpenskes Fractal.');
+	p=createP("Click play to start the magic. This is Sierpinski's Fractal.");
 	p.position(can.x+32.5,can.y+17.5);
 	p.class("bord");
 
 
-	left=createVector(width/8,7*height/8);
-	middle=createVector(width/2,height/8);
-	right=createVector(7*width/8,7*height/8);
+	left=createVector(width/2-200,height/2+200*sqrt(3)/2);
+	middle=createVector(width/2,height/2-200*sqrt(3)/2);
+	right=createVector(width/2+200,height/2+200*sqrt(3)/2);
 	sees.push(new sclass(left,middle,right));
 
 	createburrito();
